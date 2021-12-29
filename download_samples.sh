bf7b28e60cbd5b649f292f92e87a7da2ff298bff
Lorenzo Garuti <garuti.lorenzo@gmail.com>
2021-12-29 17:23:30 +0100

    Fix download_samples.sh

--- download_samples.sh
+++ download_samples.sh
@@ -5,12 +5,15 @@ DATA_PATH='data/'
 
 for sample in workplace.meta.stackexchange.com.7z unix.meta.stackexchange.com.7z
 do 
+    if [[ $sample == *".meta"* ]]; then
+       base_name=$(echo $sample | sed 's/.meta.stackexchange.com.7z//')
+    else
+       base_name=$(echo $sample | sed 's/.stackexchange.com.7z//')
+    fi
     full_url=$BASE_URL$sample
-    base_name=$(echo $sample | sed 's/.meta.stackexchange.com.7z//')
     echo $full_url
     echo $base_name
     mkdir -p $DATA_PATH/$base_name
     curl -L $full_url -o $DATA_PATH/$base_name/$sample
     7za e  -o$DATA_PATH$base_name/ $DATA_PATH$base_name/$sample
-
 done
